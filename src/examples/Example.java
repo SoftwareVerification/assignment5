5188fd5809168f402dfe77c50a3a3cbcc0c09ccc
Hendrikto <hendrik.to@gmail.com>
2017-06-01 14:51:49 +0200

    Remove redundant modifiers.

--- src/examples/Example.java
+++ src/examples/Example.java
@@ -10,20 +10,20 @@ import ast.type.TypeVar;
  */
 public interface Example {
     // (\x -> \y -> x y)(\x -> x)
-    public static final Exp prog1 =
+    Exp prog1 =
             new App(new Abs("x", new Abs("y", new App(new Var("x"), new Var("y")))),
                     new Abs("x", new Var("x")));
     // (\x -> \y -> x z)
-    public static final Exp prog2 =
+    Exp prog2 =
             new Abs("x", new Abs("y", new Abs("z", new App(new App(new Var("x"), new Var("z")), new App(new Var("y"), new Var("z"))))));
-    public static final Exp prog3 = new Abs("x", new App(new Abs("x", new Var("x")), new Var("x")));
-    public static final Exp prog4 = new Abs("x", new BinExp(BinOp.opLt, new Var("x"), new Var("x")));
-    public static final Type type1 =
+    Exp prog3 = new Abs("x", new App(new Abs("x", new Var("x")), new Var("x")));
+    Exp prog4 = new Abs("x", new BinExp(BinOp.opLt, new Var("x"), new Var("x")));
+    Type type1 =
             new TypeArrow(new TypeVar(0), new TypeArrow(new TypeVar(0), new TypeVar(1)));
-    public static final Type type2 =
+    Type type2 =
             new TypeArrow(new TypeArrow(new TypeVar(1), new TypeVar(1)), new TypeVar(2));
-    public static final Type type3 =
+    Type type3 =
             new TypeArrow(new TypeVar(0), new TypeVar(1));
-    public static final Type type4 =
+    Type type4 =
             new TypeVar(0);
 }
